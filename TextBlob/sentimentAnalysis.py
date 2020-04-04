6b487a3d0122152523f2b7f485fec5aeeeeede0c
JIUNING ZHONG <johnny0109@gmail.com>
2020-04-04 13:10:15 -0400

    revise csv generator

--- TextBlob/sentimentAnalysis.py
+++ TextBlob/sentimentAnalysis.py
@@ -37,6 +37,24 @@ complexity_dictE = Dict()
 complexity_dictEF = Dict()
 complexity_dictF = Dict()
 
+L_I_counter = 0
+L_Q_counter = 0
+L_R_counter = 0
+L_IS_counter = 0
+
+L_EE_counter = 0
+L_E_counter = 0
+L_EF_counter = 0
+L_F_counter = 0
+
+L_X_counter = 0
+L_T_counter = 0
+
+L_RS_counter = 0
+L_RF_counter = 0
+
+L_QS_counter = 0
+L_QF_counter = 0
 
 def write_to_hashes_csv(dict1, dict2, file_name):
     with open(file_name, 'w', newline='', encoding="utf8") as csvfile:
@@ -65,25 +83,34 @@ def write_four_types_to_csv(dict1, dict2, file_name):
         for k in keys:
             writer.writerow((dict1[k], dict2[k]))
 
+def strip_html_tags(body):
+    regex = re.compile('<.*?>')
+    return re.sub(regex, '', body)
 
 def clean_text(text):
     '''Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.'''
     text = text.lower()
-    text = re.sub(r'!"#%&(),:;<=>@]_`}~', '', text)  # *+/
+    text = re.sub(r'!#%&,:;<=>@]_`}~', '', text)  # *+/
 
     # text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
     # text = re.sub(r'\w*\d\w*', '', text)
     # text = re.sub(r'http\S+', 'urllink', text, flags=re.S)
 
     # remove http/https, punctuation, and remove space at the beginning and the end
-    text = text.replace('/', ' ').replace('<', ' ').replace('>', ' ').replace(';', ' ').replace(':', ' ') \
-        .replace(')', ' ').replace('!', ' ').replace('?', ' ').replace('.', ' ').replace(',', ' ') \
-        .replace('[', '').replace(']', '').replace('-', '').replace('(', ' ') \
-        .replace('$', ' ').replace('\n', '').replace('\t', '').replace('\r', '')
+    text = text.replace('/', ' ').replace('<', ' ').replace('>', ' ').replace(';', ' ') \
+        .replace('.', ' ').replace(',', ' ').replace('[', '').replace(']', '').replace('-', '') \
+        .replace('$', ' ').replace('\n', '').replace('\t', '').replace('\r', '')\
+        .replace('=', ' ').replace('&', ' ').replace('+', ' ').replace('_', ' ')\
+        .replace('%', ' ').replace('"', ' ').replace('*', ' ').replace('#', ' ')\
+        .replace('attachments', ' ').replace('albany', ' ').replace('kf', ' ')\
+        .replace('df', ' ').replace('gif', ' ').replace('png', ' ').replace('edu ', ' ')\
+        .replace('com ', ' ').replace('https', ' ').replace('rit ', ' ').replace('www', ' ').replace('http', ' ')
+        #.replace(':', ' ').replace(')', ' ').replace('(', ' ')
+        # .replace('!', ' ').replace('?', ' ')
 
     # print('before strip():' + text)
 
-    text = (" ".join(text.split())).strip()
+    text = (" ".join(text.split())).strip().apply(strip_html_tags)
     # print('after strip():' + text)
     # split text without spaces into list of words and concatenate string in a list to a single string
     # text = ' '.join(wn.split(text))
@@ -91,7 +118,6 @@ def clean_text(text):
     # spell check
 
     return text
-
 
 def read_four_types_from_csv(file_name):
     with open(file_name, mode='r', encoding="utf8") as csv_file:
@@ -160,6 +186,25 @@ def process_four_csv_file(file_name):
 
 
 def read_from_csv(file_name):
+    global L_I_counter
+    global L_Q_counter
+    global L_R_counter
+    global L_IS_counter
+
+    global L_EE_counter
+    global L_E_counter
+    global L_EF_counter
+    global L_F_counter
+
+    global L_X_counter
+    global L_T_counter
+
+    global L_RS_counter
+    global L_RF_counter
+
+    global L_QS_counter
+    global L_QF_counter
+
     with open(file_name, mode='r', encoding="utf8") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         line_count = 0
@@ -174,6 +219,18 @@ def read_from_csv(file_name):
 
             text_with_dup_dict[line_count] = tmp
             line_count += 1
+
+            # all-type          row[0]
+            # text              row[1]
+            # Label             row[2]
+            # ID                row[3]
+            # Title             row[4]
+            # Authors           row[5]
+            # Created           row[6]
+            # Unit	            row[7]
+            # Class             row[8]
+            # Subject(Thread)   row[9]
+            # scientificness    row[10]
 
             label = row[2]
             if 'Train_all_types.csv' in file_name or 'Train_KF2.csv' in file_name:
@@ -206,6 +263,35 @@ def read_from_csv(file_name):
                 else:
                     complexity_dict[line_count] = label
 
+                if label == 'L-I':
+                    L_I_counter = L_I_counter+1
+                elif label == 'L-Q':
+                    L_Q_counter = L_Q_counter+1
+                elif label == 'L-R':
+                    L_R_counter = L_R_counter+1
+                elif label == 'L-IS':
+                    L_IS_counter = L_IS_counter+1
+                elif label == 'L-EE':
+                    L_EE_counter = L_EE_counter+1
+                elif label == 'L-E':
+                    L_E_counter = L_E_counter+1
+                elif label == 'L-F':
+                    L_F_counter = L_F_counter+1
+                elif label == 'L-EF':
+                    L_EF_counter = L_EF_counter+1
+                elif label == 'L-T':
+                    L_T_counter = L_T_counter+1
+                elif label == 'L-X':
+                    L_X_counter = L_X_counter+1
+                elif label == 'L-RS':
+                    L_RS_counter = L_RS_counter+1
+                elif label == 'L-RF':
+                    L_RF_counter = L_RF_counter+1
+                elif label == 'L-QF':
+                    L_QF_counter = L_QF_counter+1
+                elif label == 'L-QS':
+                    L_QS_counter = L_QS_counter+1
+
                 testimonial = TextBlob(tmp)
                 polarity_dict[line_count] = testimonial.sentiment.polarity
                 subjectivity_dict[line_count] = testimonial.sentiment.subjectivity
@@ -218,11 +304,15 @@ def read_from_file(file_name):
 
 
 def process_csv_file(file_name):
+
     d = os.getcwd()
+
     text_dict.clear()
     complexity_dict.clear()
     text_with_dup_dict.clear()
+
     textPath = path.join(d, file_name)
+
     read_from_csv(textPath)
 
     print(f'---------------------{file_name}------------------------')
@@ -250,6 +340,22 @@ if __name__ == "__main__":
     process_csv_file('Train_KF2.csv')
     process_csv_file('Train_question.csv')
     process_csv_file('Train_resource.csv')
+
+    print('L_I_counter: ' + str(L_I_counter))
+    print('L_Q_counter: ' + str(L_Q_counter))
+    print('L_R_counter: ' + str(L_R_counter))
+    print('L_IS_counter: ' + str(L_IS_counter))
+
+    print('L_EE_counter: ' + str(L_EE_counter))
+    print('L_E_counter: ' + str(L_E_counter))
+    print('L_EF_counter: ' + str(L_EF_counter))
+    print('L_F_counter: ' + str(L_F_counter))
+    print('L_X_counter: ' + str(L_X_counter))
+    print('L_T_counter: ' + str(L_T_counter))
+    print('L_RF_counter: ' + str(L_RF_counter))
+    print('L_RS_counter: ' + str(L_RS_counter))
+    print('L_QF_counter: ' + str(L_QF_counter))
+    print('L_QS_counter: ' + str(L_QS_counter))
 
     # process_csv_file('light_data_v01.csv')
 
